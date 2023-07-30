ca5f67d19c8738016380faa1aa778bbfbec21b14
vineeth <119692597+vineeth38@users.noreply.github.com>
2023-07-30 15:02:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 const byteSize = (str) => {
   // write your code here
+	if (!inputString || inputString.length === 0) {
+    return 0;
+  }
+
+  let totalSize = 0;
+  for (let i = 0; i < inputString.length; i++) {
+    const charCode = inputString.charCodeAt(i);
+    // For characters outside the ASCII range (128 and above), add 2 bytes (UTF-8 encoding).
+    totalSize += charCode < 128 ? 1 : 2;
+  }
+
+  return totalSize;
 };
 
 // Do not change the code below
